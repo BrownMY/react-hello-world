4eb421e7ab0481c1408182c081b58c5dda5372a3
mbrown <tugtuj@gmail.com>
2021-02-23 14:26:17 -0500

    App - Imported Car

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //imports here
 import Animal from './Animal'
 import Human from './Human'
+import Car from './Car'
 
 function App() {
   return (
@@ -8,10 +9,10 @@ function App() {
       <h1>Title</h1>
       
       <Human name="Mocha" location="Detroit"/>
-      <h3>Mocha has 2 animals.</h3>
+      <h3>Mocha has 2 animals:</h3>
       <Animal animal="Cat" name="Teddy"/>
       <Animal animal="Cat" name="Gwendolyn"/>
-
+      <Car />
     </div>
   );
 }
