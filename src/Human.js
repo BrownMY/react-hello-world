f0a170e7363d1ea9fe9a942b6e12ca479df3d5de
mbrown <tugtuj@gmail.com>
2021-02-23 14:27:27 -0500

    Human - Imported Animal/added animal prop "age"

--- src/Human.js
+++ src/Human.js
@@ -1,9 +1,13 @@
+import Animal from './Animal'
+
 function Human(props) {
+
     return (
         <div>
             <h1>Mocha</h1>
             <p>Name: {props.name}</p>
             <p>Location: {props.location}</p>
+            <Animal age="6"/>
         </div>
     )
 }
